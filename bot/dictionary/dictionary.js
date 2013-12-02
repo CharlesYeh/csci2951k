d15048c8c20996ce97bbd7a63ea4772f72040651
CharlesYeh <charles_yeh@brown.edu>
2013-12-02 00:33:02 -0500

    dictionary edits

--- bot/dictionary/dictionary.js
+++ bot/dictionary/dictionary.js
@@ -21,23 +21,34 @@ lookupWord = function(bot, word, rep) {
   dict = {
     "say": function() { return new actions.ActionChat(bot, rep) },
 
-    "move": function() { return new actions.ActionMove(bot, rep) },
-    "walk": function() { return new actions.ActionMove(bot, rep) },
-    "run": function() { return new actions.ActionMove(bot, rep) },
-    "jump": function() { return new actions.ActionMove(bot, rep) },
-    "shuffle": function() { return new actions.ActionMove(bot, rep) },
+    "move": 	new actions.ActionMove(bot, rep),
+    "walk": 	new actions.ActionMove(bot, rep),
+    "run": 		new actions.ActionMove(bot, rep),
+    "jump": 	new actions.ActionMove(bot, rep),
+    "shuffle": 	new actions.ActionMove(bot, rep),
 
-    "look": function() { return new actions.ActionLook(bot, rep) },
-    "turn": function() { return new actions.ActionLook(bot, rep) },
+    "look": 	new actions.ActionLook(bot, rep),
+    "turn": 	new actions.ActionLook(bot, rep),
 
-    "mine": function() { return new actions.ActionMine(bot, rep) },
+    "mine": 	new actions.ActionMine(bot, rep),
     
-    "quickly": function() { return mod.combine(new modmove.ModMove(true, null)); },
-    "fast": function() { return mod.combine(new modmove.ModMove(true, null)); },
-    "speedily": function() { return mod.combine(new modmove.ModMove(true, null)); }
+    "quickly": 	mod.combine(new modmove.ModMove(true, null)),
+    "fast": 	mod.combine(new modmove.ModMove(true, null)),
+    "speedily": mod.combine(new modmove.ModMove(true, null))
   }
 
   return dict[word]();
+}
+
+lookupRecursively = function(bot, word, rep) {
+  var w = lookupWord(bot, word, rep);
+  for (var key in w) {
+	if (key == "fun") {
+	}
+	else {
+	  lookupRecursively(bot, word, rep);
+	}
+  }
 }
 
 exports.lookupWord = lookupWord;
