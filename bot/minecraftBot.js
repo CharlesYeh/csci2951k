2edf3ed16873688fbc60840ef336107cd2add730
EC2 <charles@eactiv.com>
2013-12-10 00:33:22 +0000

    removed astar

--- bot/minecraftBot.js
+++ bot/minecraftBot.js
@@ -1,8 +1,6 @@
 var ActionQueue = require('./planner/actionQueue');
 var pk_planner = require('./planner/planner');
 var pk_mineflayer = require('../../mineflayer/');
-var pk_astar = require('../javascript-astar/astar');
-var pk_graph = require('../javascript-astar/graph');
 
 var mcbot;
 
@@ -38,7 +36,6 @@ function MinecraftBot() {
   this.bot.nearbyBlocks = new Array();
   this.bot.scanNearby = mcbot.scanNearby;
   this.bot.getBlockType = mcbot.getBlockType;
-  this.bot.astar = mcbot.astar;
 }
 
 MinecraftBot.prototype.executeCommand = function(command) {
@@ -61,11 +58,8 @@ MinecraftBot.prototype.scanNearby = function(bot) {
   var blkPos = new pk_mineflayer.vec3();
 
   bot.nearbyBlocks = new Array();
-  grid = new Array(DIFF * 2 + 1);
 
   for (var dx = -DIFF; dx <= DIFF; dx++) {
-    grid[dx + DIFF] = new Array(DIFF * 2 + 1);
-
     for (var dz = -DIFF; dz <= DIFF; dz++) {
       blkPos.x = pos.x + dx;
       blkPos.y = pos.y;
@@ -75,19 +69,8 @@ MinecraftBot.prototype.scanNearby = function(bot) {
       if (blk != null && blk.name != 'air') {
         bot.nearbyBlocks.push(blk);
       }
-
-      if (blk.boundingBox == "block") {
-        grid[dx + DIFF][dz + DIFF] = 0;
-      }
-      else {
-        grid[dx + DIFF][dz + DIFF] = 1;
-      }
     }
   }
-  
-  bot.map = new pk_graph.Graph(grid);
-  bot.mapX = pos.x - DIFF;
-  bot.mapZ = pos.z - DIFF;
 }
 
 /**
@@ -104,38 +87,6 @@ MinecraftBot.prototype.getBlockType = function(bot, types) {
   }
 
   return matches;
-}
-
-/**
- * returns an array of actions
- */
-MinecraftBot.prototype.astar = function(bot, start, end) {
-  bot.scanNearby(bot);
-
-  var sx = Math.floor(start.x) - bot.mapX;
-  var sz = Math.floor(start.z) - bot.mapZ;
-  var ptStart = bot.map.nodes[sx][sz];
-
-  var ex = Math.floor(end.x) - bot.mapX;
-  var ez = Math.floor(end.z) - bot.mapZ;
-  var ptEnd = bot.map.nodes[ex][ez];
-  var endBlock = ptEnd.type;
-  ptEnd.type = 1;
-
-  var path = pk_astar.search(bot.map.nodes, ptStart, ptEnd, false);
-  console.log("ASTAR:  ");
-
-  var transPath = new Array();
-  for (var i in path) {
-    var v = new pk_mineflayer.vec3();
-    v.x = path[i].x + bot.mapX;
-    v.y = start.y;
-    v.z = path[i].y + bot.mapZ;
-  }
-  console.log(transPath);
-
-  ptEnd.type = endBlock;
-  return transPath;
 }
 
 module.exports = MinecraftBot;
--- main.js
+++ main.js
@@ -5,5 +5,5 @@ var mcBot = new MinecraftBot();
 
 var stdin = process.openStdin();
 stdin.addListener("data", function(d) {
-  mcBot.executeCommand(d.toString());
+    mcBot.executeCommand(d.toString());
 });
